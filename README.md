3260897910c950fc47d7309e22032103b7e55103
Leonardo Gonzalez <leogzz0@hotmail.com>
2023-08-20 13:59:28 -0600

    ENV variables

---
+++ README.md
@@ -0,0 +1,7 @@
+# MAPSE
+
+In this project, I've build a search engine that uses filtering to reorder results. The engine will get results from the Google API, store them, then rank them based on filters defined. You'll end up with a search page and better results list.
+
+**Project Steps**
+
+* Setup a programmable search engine [Custom Search API]
--- settings.py
+++ settings.py
@@ -1,5 +1,9 @@
-SEARCH_KEY = "AIzaSyCkuJv15IMXiJj308Gu-tX8tRUBtt1e3vY"
-SEARCH_ID = "57ad62ed35a284c1b"
+import os
+search_key_env = os.environ.get("SEARCH_KEY_ENV")
+search_id_env = os.environ.get("SEARCH_ID_ENV")
+
+SEARCH_KEY = search_key_env
+SEARCH_ID = search_id_env
 COUNTRY = "MX"
 
 SEARCH_URL = "https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&q={query}&start={start}&gl=" + COUNTRY
